5e63262eacdd3cced70b673690588c66b6535910
张喜成 <codehackfox@gmail.com>
2019-03-24 12:06:41 +0800

    clean code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mac-clean
 mac一键清理工具
+
+
+```shell
+sudo python main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 
-print(os.getcwd())
-print(os.system("ls -lh"))
 
-os.system("sudo rm -f  /System/Library/Speech/Voices/*")
-os.system("sudo rm -rf /private/var/log/*")
-os.system("sudo rm -rf /private/var/folders/")
-os.system("rm -rf /private/var/tmp/TM*")
-os.system("rm -rf  ~/Library/Caches/*")
+def clean(path):
+    print("before clean:",'=='*20)
+    os.system("sudo du -sh " + path)
+    os.system("sudo rm -rf " + path)
+    print("after clean:")
+    os.system("sudo du -sh " + path)
+
+
+if __name__ == "__main__":
+    paths = ["/System/Library/Speech/Voices/", "/private/var/log/",
+             "/private/var/folders/", "/private/var/tmp/TM*", "~/Library/Caches/"]
+    for path in paths:
+        clean(path)
