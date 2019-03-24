6f6d2239e993a82e497f7185c39233ca9b6194b5
张喜成 <codehackfox@gmail.com>
2019-03-24 12:43:45 +0800

    arch chagne

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
 import os
+import getopt
 
 
-def clean(path):
-    print("before clean:",'==='*20)
+def clean_path(path):
+    print("before clean:", '==='*20)
     os.system("sudo du -sh " + path)
     os.system("sudo rm -rf " + path)
     print("after clean:")
     os.system("sudo du -sh " + path)
 
 
-if __name__ == "__main__":
+def clean():
     paths = ["/System/Library/Speech/Voices/", "/private/var/log/",
              "/private/var/folders/", "/private/var/tmp/TM*", "~/Library/Caches/"]
     for path in paths:
-        clean(path)
+        clean_path(path)
+
+
+def disk_total():
+    print('loading ...')
+    os.system("sudo du -sh /*")
+    print("===="*20)
+    os.system("sudo df -h")
+
+
+def print_menu():
+    print("""
+        *************************
+        a : 自定义目录清洗
+        c : 开始自动清洗
+        d : 查询磁盘空间
+        q : 退出
+        *************************
+    """)
+
+
+def menu():
+    print_menu()
+    char = input("请选择: ")
+    while char != "q":
+        if char == 'a':
+            path = input("请输入路径:")
+            clean_path(path)
+        elif char == 'c':
+            clean()
+        elif char == 'd':
+            disk_total()
+        else:
+            print("没有此选项!")
+        print_menu()
+        char = input("请选择: ")
+    print('By!')
+
+if __name__ == "__main__":
+    menu()
