07797e68b213a0567e2e041a41b73797874584e8
张喜成 <codehackfox@gmail.com>
2019-03-24 11:36:51 +0800

    add clearn dir

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import os
 
 print(os.getcwd())
 print(os.system("ls -lh"))
+
+os.system("sudo rm -f  /System/Library/Speech/Voices/*")
+os.system("sudo rm -rf /private/var/log/*")
+os.system("sudo rm -rf /private/var/folders/")
+os.system("rm -rf /private/var/tmp/TM*")
+os.system("rm -rf  ~/Library/Caches/*")
